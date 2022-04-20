63e8aa9231c8e4c1f79c6f67f643daf24f50bb3b
Gurjinder Singh <rockygsingh@icloud.com>
2022-04-19 22:20:10 -0400

    added css properties and imported the logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,14 @@ import { Navbar, Welcome, Footer, Services, Transactions  } from "./components"
 const App = () => {
 
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>Hello Vite + React!</p>
-      </header>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+          <Navbar />
+          <Welcome />
+      </div>
+          <Services />
+          <Transactions />
+          <Footer />
     </div>
   )
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,33 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+
+
+
+import logo from '../../images/logo.png';
+
+
+const NavbarItem = ({ title, classProps }) => {
+    return (
+        <li>
+            
+        </li>
+    )
+}
+
+
 const Navbar = () => {
     return (
-        <h1>NavBar</h1>
+        <nav className='w-full flex md:justify-center justify-between items-center p-4'>
+            <div className='md:flex-[0.5] flex-initial justify-center items-center'>
+                <img src={logo} alt='logo' className='w-32 cursor-pointer' />
+                <ul className='text-white md:flex hiddent list-none flex-row justify-between items-center flex-initail'>
+
+
+                </ul>
+
+            </div>
+
+        </nav>
     );
 }
 
