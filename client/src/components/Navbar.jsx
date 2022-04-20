5a1dd35a7b317bbc45b190d84295f250cd9243af
Gurjinder Singh <rockygsingh@icloud.com>
2022-04-19 23:00:09 -0400

    created NavbarItem function to render in the Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,8 +8,8 @@ import logo from '../../images/logo.png';
 
 const NavbarItem = ({ title, classProps }) => {
     return (
-        <li>
-            
+        <li className={`mx-4 cursor-pointer ${classProps}`}>
+            {title}
         </li>
     )
 }
@@ -20,12 +20,17 @@ const Navbar = () => {
         <nav className='w-full flex md:justify-center justify-between items-center p-4'>
             <div className='md:flex-[0.5] flex-initial justify-center items-center'>
                 <img src={logo} alt='logo' className='w-32 cursor-pointer' />
-                <ul className='text-white md:flex hiddent list-none flex-row justify-between items-center flex-initail'>
+                </div>
+                <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initail'>
 
-
+                    {/* why is this array not rendering */}
+                    {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+                            <NavbarItem key={item + index} title={item} />
+                    ))}
+                    <li className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'>
+                            Login
+                    </li>
                 </ul>
-
-            </div>
 
         </nav>
     );
