2347a6a570ced0ba233b4a7b5e0584740a2f5c27
Gurjinder Singh <rockygsingh@icloud.com>
2022-04-19 21:43:58 -0400

    set up the client and smart_contract for the project

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+
+import { Navbar, Welcome, Footer, Services, Transactions  } from "./components"
+
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>Hello Vite + React!</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
